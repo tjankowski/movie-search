40f95050684f3d7b7d1e2c14772e5e549052d011
Tomek Jankowski <tomasz.jankowski@idea-inspired.com>
2020-11-09 08:20:28 +0100

    Add searching page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,124 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useRef, useCallback } from "react";
+import Link from "next/link";
+import debounce from "lodash.debounce";
+import { motion } from "framer-motion";
+import { updateUrlQuery } from "utils";
+import { search } from "api/omdb";
+import Layout from "components/Layout";
+import MovieCard from "components/MovieCard";
+import SearchForm from "components/SearchForm";
+import MovieSkeleton from "components/MovieSkeleton";
+import useIntersectionObserver from "hooks/useIntersectionObserver";
+import useMovieData from "hooks/useMovieData";
+import { fadeIn, hidden } from "styles/animations";
+import styles from "styles/Layout.module.scss";
+import resultsStyles from "styles/Results.module.scss";
+import AnimatedItem from "components/AnimatedItem";
 
-export default function Home() {
+export default function Home({ param, data }) {
+  const [query, setQuery] = useState(param);
+  const {
+    data: { pages, totalResults, totalPages, error },
+    isFetching,
+    isError,
+    actions,
+  } = useMovieData(data);
+  const bottomBoundaryRef = useRef(null);
+
+  useIntersectionObserver(bottomBoundaryRef, actions.loadMore);
+
+  const search = useCallback(debounce(actions.findMovies, 500), [
+    actions.findMovies,
+  ]);
+
+  const onQueryChange = (value) => {
+    setQuery(value);
+    search(value);
+    updateUrlQuery(value);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <motion.div
+        animate={fadeIn.animate(0)}
+        initial={fadeIn.initial}
+        exit={hidden}
+      >
+        <main className={styles.container}>
+          <h1 className={styles.header}>Looking for movie?</h1>
+          <SearchForm query={query} onChange={onQueryChange} />
+          <div className={styles.padding_h}>
+            <ul className={resultsStyles.results}>
+              {pages.flat().map((item, index) => {
+                return (
+                  <AnimatedItem
+                    className={resultsStyles.results__item}
+                    index={index}
+                    // Added index to key because API returns same movie for different pages
+                    key={`${item.imdbID}=${index}`}
+                  >
+                    <Link href={`/${encodeURIComponent(item.imdbID)}`}>
+                      <a>
+                        <MovieCard item={item} />
+                      </a>
+                    </Link>
+                  </AnimatedItem>
+                );
+              })}
+              {!isError && totalResults === 0 && (
+                <AnimatedItem className={resultsStyles.results__error}>
+                  We didn't find any results.
+                </AnimatedItem>
+              )}
+              {isError && (
+                <AnimatedItem className={resultsStyles.results__error}>
+                  {error.message}
+                </AnimatedItem>
+              )}
+              {isFetching &&
+                [...Array(3)].map((item, index) => (
+                  <AnimatedItem
+                    className={resultsStyles.results__item}
+                    index={index}
+                    key={index}
+                  >
+                    <MovieSkeleton key={index} />
+                  </AnimatedItem>
+                ))}
+            </ul>
+          </div>
+          {totalPages > 1 && <div ref={bottomBoundaryRef} />}
+        </main>
+      </motion.div>
+    </Layout>
+  );
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getServerSideProps({ query }) {
+  const params = new URLSearchParams(query);
+  const param = params.get("query") || "";
+  let data = {
+    query: param,
+    results: [],
+    totalResults: -1,
+    totalPages: 0,
+  };
+  if (param) {
+    try {
+      data = await search(param, 1);
+    } catch (error) {
+      data = {
+        query,
+        error: {
+          message: error.message,
+        },
+      };
+    }
+  }
+  return {
+    props: {
+      param,
+      data,
+    },
+  };
 }
