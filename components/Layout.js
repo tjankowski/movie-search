f9c192fd860f1764e5ed1138f65587c54c9635a4
Tomek Jankowski <tomasz.jankowski@idea-inspired.com>
2020-11-13 00:36:52 +0100

    Add missing meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,10 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Movie search</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Movie searcher" />
+        <meta name="keywords" content="Movies, Series, Games" />
+        <meta name="author" content="Tomasz Jankowski" />
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Oswald:wght@300;600&display=swap"
